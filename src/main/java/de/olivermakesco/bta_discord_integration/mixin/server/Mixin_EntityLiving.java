824dbb39cba333fd34f91c17f9a3e5d7609fe10c
WatermelonMojito <104700587+WatermelonMojito@users.noreply.github.com>
2024-06-24 15:22:41 -0500

    Updated to 7.2pre2

--- src/main/java/de/olivermakesco/bta_discord_integration/mixin/server/Mixin_EntityLiving.java
+++ src/main/java/de/olivermakesco/bta_discord_integration/mixin/server/Mixin_EntityLiving.java
@@ -2,23 +2,23 @@ package de.olivermakesco.bta_discord_integration.mixin.server;
 
 import de.olivermakesco.bta_discord_integration.server.DiscordChatRelay;
 import net.minecraft.core.entity.Entity;
-import net.minecraft.server.entity.player.EntityPlayerMP;
+import net.minecraft.core.entity.EntityLiving;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = EntityPlayerMP.class, remap = false)
-public abstract class Mixin_EntityPlayerMP {
-    @Shadow protected abstract String deathMessage(Entity entity);
+@Mixin(value = EntityLiving.class, remap = false)
+public abstract class Mixin_EntityLiving {
+    @Shadow protected abstract String getDeathMessage(Entity entity);
 
     @Inject(
             method = "onDeath",
             at = @At("RETURN")
     )
     void processDeathMessage(Entity entity, CallbackInfo ci) {
-        String message = deathMessage(entity).replaceAll("§.", "");
+        String message = getDeathMessage(entity).replaceAll("§.", "");
         DiscordChatRelay.sendDeathMessage(message);
     }
 }
