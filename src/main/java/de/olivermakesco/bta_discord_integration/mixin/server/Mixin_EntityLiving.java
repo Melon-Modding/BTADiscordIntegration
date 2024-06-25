1c358e4d2a99359aca5cf71bf2ae48993d9fe89d
WatermelonMojito <104700587+WatermelonMojito@users.noreply.github.com>
2024-06-24 23:27:54 -0500

    fixes entity death message bug

--- src/main/java/de/olivermakesco/bta_discord_integration/mixin/server/Mixin_EntityLiving.java
+++ src/main/java/de/olivermakesco/bta_discord_integration/mixin/server/Mixin_EntityLiving.java
@@ -3,6 +3,8 @@ package de.olivermakesco.bta_discord_integration.mixin.server;
 import de.olivermakesco.bta_discord_integration.server.DiscordChatRelay;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.EntityLiving;
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.server.entity.player.EntityPlayerMP;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,12 +15,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class Mixin_EntityLiving {
     @Shadow protected abstract String getDeathMessage(Entity entity);
 
+    @Shadow public boolean isMultiplayerEntity;
+
+    @Shadow public abstract String getDisplayName();
+
+    @Shadow public abstract int getMaxHealth();
+
     @Inject(
             method = "onDeath",
             at = @At("RETURN")
     )
     void processDeathMessage(Entity entity, CallbackInfo ci) {
-        String message = getDeathMessage(entity).replaceAll("§.", "");
-        DiscordChatRelay.sendDeathMessage(message);
+        if((EntityLiving)((Object)this) instanceof EntityPlayer) {
+            String message = getDeathMessage(entity).replaceAll("§.", "");
+            DiscordChatRelay.sendDeathMessage(message);
+        }
     }
 }
